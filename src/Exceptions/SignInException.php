72b72e77545ae8a8bb6db54e468735a22ebaeca8
曾达威 <zengdawei@haxibiao.com>
2020-06-04 14:59:07 +0800

    fixed: 登录异常抛出信息 前端看不到

--- src/Exceptions/SignInException.php
+++ src/Exceptions/SignInException.php
@@ -3,11 +3,36 @@
 namespace haxibiao\user\Exceptions;
 
 use Exception;
+use Nuwave\Lighthouse\Exceptions\RendersErrorsExtensions;
 
-class SignInException extends Exception
+class SignInException extends Exception implements RendersErrorsExtensions
 {
+    /**
+     * @var @string
+     */
+    private $reason;
+
     public function __construct($msg = '', $code = 1)
     {
         parent::__construct($msg, $code);
+        $this->reason = $msg;
+    }
+
+    public function isClientSafe(): bool
+    {
+        return true;
+    }
+
+    public function getCategory(): string
+    {
+        return '登录异常';
+    }
+
+    public function extensionsContent(): array
+    {
+        return [
+            'some'   => '后端登录检测异常',
+            'reason' => $this->reason,
+        ];
     }
 }
