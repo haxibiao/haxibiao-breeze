35b573b36dfb50ea8ae184209899b768c50f00f0
龚国玮 <gongguowei@haxifang.com>
2020-07-08 23:12:02 +0800

    fix:UserModel缺少follows关系

--- src/User.php
+++ src/User.php
@@ -2,10 +2,12 @@
 
 namespace Haxibiao\Base;
 
+use App\Follow;
 use Haxibiao\Base\Traits\AuthHelper;
 use Haxibiao\Base\Traits\AvatarHelper;
 use Haxibiao\Base\Traits\UserResolvers;
 use Haxibiao\Base\UserRetention;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as BaseUser;
 
@@ -101,6 +103,12 @@ class User extends BaseUser
     public function user_retention(): HasOne
     {
         return $this->hasOne(UserRetention::class);
+    }
+
+    // package/content 中使用getUser()获取到了该User，但是该User缺少follows关系
+    public function follows(): HasMany
+    {
+        return $this->hasMany(Follow::class);
     }
 
     //属性
