a5fe5864803424218b592b4188a4e05ef680023a
ivan <ivan@mac>
2020-12-26 11:18:32 +0800

    删掉重复的基础model方法

--- src/Model.php
+++ src/Model.php
@@ -8,29 +8,4 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 class Model extends BaseModel
 {
     use ModelHelpers;
-
-    /*
-     * 查询时排除相关列,减少传输消耗
-     * @param  [type] $query [description]
-     * @param  array  $value [description]
-     * @return [type]        [description]
-     */
-    public function scopeExclude($query, $value = array())
-    {
-        //获取该表所有列
-        $columns = $this->getTableColumns();
-        //需要获取列名
-        $real_columns = array_diff($columns, (array) $value);
-
-        $tableName      = $this->getTable();
-        $format_colomns = array_map(function ($name) use ($tableName) {
-            return $tableName . '.' . $name;
-        }, $real_columns);
-        return $query->select($format_colomns);
-    }
-
-    public function getTableColumns()
-    {
-        return $this->getConnection()->getSchemaBuilder()->getColumnListing($this->getTable());
-    }
 }
