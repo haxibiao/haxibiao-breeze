9190e151dcbece96ea3ef40bc115fe5414e4a5c5
ivan <ivan@haxibiao.com>
2021-01-23 19:55:03 +0800

    breeze seed 3个基本角色的用户

--- src/Seeders/UserSeeder.php
+++ src/Seeders/UserSeeder.php
@@ -1,7 +1,6 @@
 <?php
 namespace Haxibiao\Breeze\Seeders;
 
-use App\Profile;
 use App\User;
 use Illuminate\Database\Seeder;
 
@@ -21,13 +20,14 @@ class UserSeeder extends Seeder
             dd("请设置 网站中文名称 env('APP_NAME_CN')");
         }
 
-        //删除荣誉的admin新账户
         $admin_email = env('MAIL_USERNAME');
-        foreach (User::whereEmail($admin_email)->get() as $admin) {
-            if ($admin->id > 1) {
-                $admin->delete();
-            }
-        }
+
+        // //删除冗余的admin新账户？
+        // foreach (User::whereEmail($admin_email)->get() as $admin) {
+        //     if ($admin->id > 1) {
+        //         $admin->delete();
+        //     }
+        // }
 
         //锁定id=1为super admin
         $user = User::find(1);
@@ -44,13 +44,43 @@ class UserSeeder extends Seeder
 
         $user->password  = bcrypt($pass);
         $user->api_token = str_random(60);
-        $user->role_id   = 2; //管理员
+        $user->role_id   = User::ADMIN_STATUS; //管理员
         $user->save();
-        $profile = $user->profile;
-        if (empty($profile)) {
-            $profile          = new Profile();
-            $profile->user_id = $user->id;
+        //profile 属性会自动lazy load ...
+
+        //锁定id=2 为 小编
+        $editor = User::find(2);
+        if (!$editor) {
+            $editor = User::firstOrNew([
+                'email' => $admin_email,
+            ]);
         }
-        $profile->save();
+        $editor->email     = 'editor@breeze.com';
+        $editor->account   = $user->email;
+        $editor->phone     = $user->email;
+        $editor->name      = env('APP_NAME_CN') . "小编";
+        $pass              = env('DB_PASSWORD', 'REDIS_PASSWORD');
+        $editor->password  = bcrypt($pass);
+        $editor->api_token = str_random(60);
+        $editor->role_id   = User::EDITOR_STATUS; //小编
+        $editor->save();
+
+        //锁定id=3 为 测试用户
+        $user = User::find(3);
+        if (!$user) {
+            $user = User::firstOrNew([
+                'email' => $admin_email,
+            ]);
+        }
+        $user->email     = 'user@breeze.com';
+        $user->account   = $user->email;
+        $user->phone     = $user->email;
+        $user->name      = env('APP_NAME_CN') . "用户";
+        $pass            = env('DB_PASSWORD', 'REDIS_PASSWORD');
+        $user->password  = bcrypt($pass);
+        $user->api_token = str_random(60);
+        //role_id 默认为普通用户
+        $user->save();
+
     }
 }
