aaccfd551d03569d407f207df306cef1a3476731
xxm <xiaoxinming@haxibiao.com>
2021-01-28 23:15:04 +0800

    fixed:修复retention记录不存在,报错。

--- src/UserRetention.php
+++ src/UserRetention.php
@@ -38,44 +38,46 @@ class UserRetention extends Model
             return;
         }
 
-        try {
+        $retention = $user->retention;
+        if (!is_null($retention)) {
             //baseUser 有唯一留存档案属性(lazy load)
-            $retention = $user->retention;
-
-            $diffDay = today()->startOfDay()->diffInDays($user->created_at->startOfDay());
-            $diffDay = $diffDay + 1; //次留算day2
-            $diffDay = $diffDay >= 29 ? 30 : $diffDay; //超过29天能活跃，就算月留存
-            switch ($diffDay) {
-                case 2: //次日留
-                    $retention->day2_at = $retention->day2_at ?? now();
-                    break;
-                case 3: //3日留
-                    $retention->day3_at = $retention->day3_at ?? now();
-                    break;
-                case 4:
-                    $retention->day4_at = $retention->day4_at ?? now();
-                    break;
-                case 5: //5日留
-                    $retention->day5_at = $retention->day5_at ?? now();
-                    break;
-                case 6:
-                    $retention->day6_at = $retention->day6_at ?? now();
-                    break;
-                case 7: //7日留
-                    $retention->day7_at = $retention->day7_at ?? now();
-                    break;
-                case 15: //15日留
-                    $retention->day15_at = $retention->day15_at ?? now();
-                    break;
-                case 30: //月留
-                    $retention->day30_at = $retention->day30_at ?? now();
-                    break;
-                default:
-                    break;
+            try {
+                $diffDay = today()->startOfDay()->diffInDays($user->created_at->startOfDay());
+                $diffDay = $diffDay + 1; //次留算day2
+                $diffDay = $diffDay >= 29 ? 30 : $diffDay; //超过29天能活跃，就算月留存
+                switch ($diffDay) {
+                    case 2: //次日留
+                        $retention->day2_at = $retention->day2_at ?? now();
+                        break;
+                    case 3: //3日留
+                        $retention->day3_at = $retention->day3_at ?? now();
+                        break;
+                    case 4:
+                        $retention->day4_at = $retention->day4_at ?? now();
+                        break;
+                    case 5: //5日留
+                        $retention->day5_at = $retention->day5_at ?? now();
+                        break;
+                    case 6:
+                        $retention->day6_at = $retention->day6_at ?? now();
+                        break;
+                    case 7: //7日留
+                        $retention->day7_at = $retention->day7_at ?? now();
+                        break;
+                    case 15: //15日留
+                        $retention->day15_at = $retention->day15_at ?? now();
+                        break;
+                    case 30: //月留
+                        $retention->day30_at = $retention->day30_at ?? now();
+                        break;
+                    default:
+                        break;
+                }
+                $retention->save();
+            } catch (\Exception $ex) {
+                //
             }
-            $retention->save();
-        } catch (\Exception $ex) {}
-
+        }
     }
 
     public static function invitedUserCacheValue($date)
