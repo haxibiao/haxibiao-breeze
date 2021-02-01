58517321579bf823eb97a6c118d3b1508be8972c
ivan <ivan@mac>
2021-02-01 19:53:27 +0800

    fix breeze 通过composer 模式加载时w无AuthServiceProvider

--- routes/auth.php
+++ routes/auth.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+//breeze通过composer 模式加载时AuthServiceProvider也许加载更晚，找不带Auth Facade..
 
 //登录注册
-Auth::routes();
+// Auth::routes();
+Route::mixin(new \Laravel\Ui\AuthRouteMethods());
 //验证邮件
-Auth::routes(['verify' => true]);
+// Auth::routes(['verify' => true]);
+Route::auth(['verify' => true]);
