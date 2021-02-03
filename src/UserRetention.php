a5f2de9665897d1144856a92edb99f61b4d578df
xxm <xiaoxinming@haxibiao.com>
2021-02-03 11:50:59 +0800

    fixed:修复留存率统计失败,加多了判断条件。

--- src/UserRetention.php
+++ src/UserRetention.php
@@ -2,7 +2,9 @@
 namespace Haxibiao\Breeze;
 
 use Haxibiao\Breeze\User;
+use Haxibiao\Breeze\UserRetention;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class UserRetention extends Model
 {
@@ -33,50 +35,49 @@ class UserRetention extends Model
      */
     public static function recordUserRetention($user)
     {
-        $user = User::find($user->id);
-        if (!$user) {
-            return;
-        }
+        // FIXED: $user->id find user ???
+        // $user = User::find($user->id);
+        // if (!$user) {
+        //     return;
+        // }
 
         $retention = $user->retention;
-        if (!is_null($retention)) {
-            //baseUser 有唯一留存档案属性(lazy load)
-            try {
-                $diffDay = today()->startOfDay()->diffInDays($user->created_at->startOfDay());
-                $diffDay = $diffDay + 1; //次留算day2
-                $diffDay = $diffDay >= 29 ? 30 : $diffDay; //超过29天能活跃，就算月留存
-                switch ($diffDay) {
-                    case 2: //次日留
-                        $retention->day2_at = $retention->day2_at ?? now();
-                        break;
-                    case 3: //3日留
-                        $retention->day3_at = $retention->day3_at ?? now();
-                        break;
-                    case 4:
-                        $retention->day4_at = $retention->day4_at ?? now();
-                        break;
-                    case 5: //5日留
-                        $retention->day5_at = $retention->day5_at ?? now();
-                        break;
-                    case 6:
-                        $retention->day6_at = $retention->day6_at ?? now();
-                        break;
-                    case 7: //7日留
-                        $retention->day7_at = $retention->day7_at ?? now();
-                        break;
-                    case 15: //15日留
-                        $retention->day15_at = $retention->day15_at ?? now();
-                        break;
-                    case 30: //月留
-                        $retention->day30_at = $retention->day30_at ?? now();
-                        break;
-                    default:
-                        break;
-                }
-                $retention->save();
-            } catch (\Exception $ex) {
-                //
+        //baseUser 有唯一留存档案属性(lazy load)
+        try {
+            $diffDay = today()->startOfDay()->diffInDays($user->created_at->startOfDay());
+            $diffDay = $diffDay + 1; //次留算day2
+            $diffDay = $diffDay >= 29 ? 30 : $diffDay; //超过29天能活跃，就算月留存
+            switch ($diffDay) {
+                case 2: //次日留
+                    $retention->day2_at = $retention->day2_at ?? now();
+                    break;
+                case 3: //3日留
+                    $retention->day3_at = $retention->day3_at ?? now();
+                    break;
+                case 4:
+                    $retention->day4_at = $retention->day4_at ?? now();
+                    break;
+                case 5: //5日留
+                    $retention->day5_at = $retention->day5_at ?? now();
+                    break;
+                case 6:
+                    $retention->day6_at = $retention->day6_at ?? now();
+                    break;
+                case 7: //7日留
+                    $retention->day7_at = $retention->day7_at ?? now();
+                    break;
+                case 15: //15日留
+                    $retention->day15_at = $retention->day15_at ?? now();
+                    break;
+                case 30: //月留
+                    $retention->day30_at = $retention->day30_at ?? now();
+                    break;
+                default:
+                    break;
             }
+            $retention->save();
+        } catch (\Exception $ex) {
+            Log::error($ex);
         }
     }
 
