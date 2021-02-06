a9e5c0139beb3ed5051d7685fa731576f0d0033b
杨倩 <yangqian@haxifang.cn>
2021-02-06 09:35:33 +0800

    up check_ins migrate

--- database/migrations/2020_06_29_120848_create_check_ins_table.php
+++ database/migrations/2020_06_29_120848_create_check_ins_table.php
@@ -13,16 +13,17 @@ class CreateCheckInsTable extends Migration
      */
     public function up()
     {
-        Schema::create('check_ins', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedInteger('user_id')->comment('用户ID');
-            $table->tinyInteger('reward_rate')->default(1)->comment('奖励倍数');
-
-            $table->unsignedInteger('gold_reward')->default(0)->comment('健康分奖励');
-            $table->unsignedInteger('contribute_reward')->default(0)->comment('精力点奖励');
-            $table->unsignedInteger('keep_checkin_days')->default(0)->comment('连续签到日');
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('check_ins')) {
+            Schema::create('check_ins', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedInteger('user_id')->comment('用户ID');
+                $table->tinyInteger('reward_rate')->default(1)->comment('奖励倍数');
+                $table->unsignedInteger('gold_reward')->default(0)->comment('健康分奖励');
+                $table->unsignedInteger('contribute_reward')->default(0)->comment('精力点奖励');
+                $table->unsignedInteger('keep_checkin_days')->default(0)->comment('连续签到日');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
