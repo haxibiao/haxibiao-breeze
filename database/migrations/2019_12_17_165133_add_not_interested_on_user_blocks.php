02cd1a583f57ee4dd9b0271580d068bb07155509
蔡志刚 <caizhigang@haxibiao.com>
2021-03-03 15:27:44 +0800

    fix artisan migrate error

--- database/migrations/2019_12_17_165133_add_not_interested_on_user_blocks.php
+++ database/migrations/2019_12_17_165133_add_not_interested_on_user_blocks.php
@@ -14,11 +14,11 @@ class AddNotInterestedOnUserBlocks extends Migration
     public function up()
     {
         Schema::table('user_blocks', function (Blueprint $table) {
-            if (!Schema::hasColumn('user_blocks', 'article_block_id')) {
+            if (Schema::hasColumn('user_blocks', 'article_block_id')) {
                 $table->bigInteger('article_block_id')->after('user_block_id')->nullable()->comment("不感兴趣的动态id");
             }
-            if (!Schema::hasColumn('user_blocks', 'article_report_id')) {
-                $table->Integer('article_report_id')->after('user_block_id')->nullable()->comment("被举报的动态id");
+            if (Schema::hasColumn('user_blocks', 'article_report_id')) {
+                $table->Integer('article_report_id')->nullable()->comment("被举报的动态id");
             }
             if (Schema::hasColumn('user_blocks', 'user_block_id')) {
                 $table->bigInteger('user_block_id')->nullable()->change();
