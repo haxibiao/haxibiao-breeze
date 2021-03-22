07a851c5fc655961f656f1d7abd319f48fd457b7
ivan <ivan@haxibiao.com>
2021-03-22 16:56:34 +0800

    恢复assets代码，准备重构支持video player 的hls支持短视频播放

---
+++ resources/assets/js/components/follow/Timeline.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+   <ul class="time-line">
+		<article-list api="/" />
+   </ul>
+</div>
+</template>
+
+<script>
+export default {
+
+  name: 'Timeline',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
