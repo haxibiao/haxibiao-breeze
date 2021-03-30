1119ef3d9d3c4ab8feb340dd918fa77ffe5a2ae1
张博 <zhangbo@haxifang.cn>
2021-03-30 14:31:23 +0800

    fix 用户粉丝和关注列表

--- resources/views/user/parts/follow_item.blade.php
+++ resources/views/user/parts/follow_item.blade.php
@@ -1,5 +1,5 @@
 <li class="user-info info-md">
-  if($user){
+  @if($user)
   <a class="avatar" href="/user/{{ $user->id }}"><img src="{{ $user->avatarUrl }}" alt=""></a>
 
   <follow type="users" id="{{ $user->id }}" user-id="{{ user_id() }}" followed="{{ is_follow('users', $user->id) }}">
@@ -16,5 +16,5 @@
       写了 {{ $user->count_words }} 字，获得了 {{ $user->count_likes }} 个喜欢
     </div>
   </div>
-  }
+  @endif
 </li>
