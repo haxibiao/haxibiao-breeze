ae98591fe59d513e8a23af7ec79679a2b98da9c3
wyk <wangyukun@haxibiao.com>
2021-04-19 15:46:06 +0800

    axios 注释补充

--- resources/assets/js/components/comment/axios.js
+++ resources/assets/js/components/comment/axios.js
@@ -13,19 +13,18 @@ const CONSOLE_REQUEST_ENABLE = true;
 function requestSuccessFunc(config) {
     CONSOLE_REQUEST_ENABLE && console.info('requestSuccessFunc', config);
     // 请求拦截逻辑，可以处理权限，请求发送监控等
-    // ...
     return config;
 }
 
 function requestFailFunc(error) {
     CONSOLE_REQUEST_ENABLE && console.info('requestFailFunc', error);
     // 发送请求失败逻辑，断网，请求发送监控等
-    // ...
     return Promise.reject(error);
 }
 
 function responseSuccessFunc(response) {
     CONSOLE_REQUEST_ENABLE && console.info('responseSuccessFunc', response);
+    // 响应成功逻辑
     response.config.successMessage &&
         GLOBAL.vueBus.$emit('global.$dialog.show', {
             response,
@@ -36,6 +35,7 @@ function responseSuccessFunc(response) {
 
 function responseFailFunc(error) {
     CONSOLE_REQUEST_ENABLE && console.info('responseFailFunc', error);
+    // 响应失败逻辑
     error.config.errorMessage &&
         GLOBAL.vueBus.$emit('global.$dialog.show', {
             error,
