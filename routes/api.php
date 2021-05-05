990a9d40b6c61cdfdc667df6ad19e16d9553be00
ivan <ivan@haxibiao.com>
2021-05-05 11:46:17 +0800

    重构media 清理代码

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,6 @@ register_routes(dirname(__FILE__) . '/api');
 // Route::post('/live/cutOut', 'Api\LiveController@cutOutLive');
 // Route::post('/live/recording', 'Api\LiveController@recording');
 
-////注释的原因：RestFul方法废弃了，现在统一用GQL解析抖音视频
-//Route::post('/article/resolverDouyin', 'Api\ArticleController@resolverDouyinVideo');
-Route::post('/media/import', 'SpiderController@importDouyinSpider');
-Route::any('/media/oldHook', 'SpiderController@hook');
-
 Route::middleware('auth:api')->post('/background', 'UserController@saveBackground');
-
-Route::post('/douyin/import', 'SpiderController@importDouYin');
 
 Route::post('/withdraw', 'WithdrawController@withdraws');
