025c1e6d77c8158cfbcde64b070e488401d0c155
zengdawei <>
2021-05-07 11:57:22 +0800

    fix app column already exists

--- database/migrations/2019_08_21_210537_add_app_column_to_users_table.php
+++ database/migrations/2019_08_21_210537_add_app_column_to_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddAppColumnToUsersTable extends Migration
 {
@@ -14,7 +14,10 @@ class AddAppColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('app',30)->nullable()->comment('APP');
+            if (!Schema::hasColumn('users', 'app')) {
+                $table->string('app', 30)->nullable()->comment('APP');
+
+            }
         });
     }
 
