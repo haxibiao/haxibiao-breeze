bca8c34edf8b12b653284395757eac201829b184
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-05-21 10:59:57 +0800

    答赚的UserException覆盖原异常类

--- src/Exceptions/UserException.php
+++ src/Exceptions/UserException.php
@@ -3,11 +3,39 @@
 namespace Haxibiao\Breeze\Exceptions;
 
 use Exception;
+use Haxibiao\Breeze\Exceptions\ErrorCode;
+use Nuwave\Lighthouse\Exceptions\RendersErrorsExtensions;
 
-class UserException extends Exception
+class UserException extends Exception implements RendersErrorsExtensions
 {
-    public function __construct($msg = '', $code = 1)
+
+    /**
+     * @var @string
+     */
+    private $reason;
+
+    public function __construct($msg = '', $code = ErrorCode::FAILURE_STATUS)
     {
         parent::__construct($msg, $code);
+        $this->reason = $msg;
+    }
+
+    public function isClientSafe(): bool
+    {
+        return true;
+    }
+
+    public function getCategory(): string
+    {
+        return '主动异常';
+    }
+
+    public function extensionsContent(): array
+    {
+        return [
+            'some'   => '后端异常' . gethostname(),
+            'reason' => $this->reason,
+            'code'   => (string) $this->code,
+        ];
     }
 }
