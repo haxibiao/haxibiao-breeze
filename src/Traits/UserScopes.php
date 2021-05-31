82860c5b94aaeaa568483f6c073cc7131841691d
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-05-31 16:05:54 +0800

    fix:use user.role error

--- src/Traits/UserScopes.php
+++ src/Traits/UserScopes.php
@@ -4,8 +4,8 @@ namespace Haxibiao\Breeze\Traits;
 
 trait UserScopes
 {
-    public function scopeRole($query, $role_id)
-    {
-        return $query->where('role_id', $role_id);
-    }
+    // public function scopeRole($query, $role_id)
+    // {
+    //     return $query->where('role_id', $role_id);
+    // }
 }
