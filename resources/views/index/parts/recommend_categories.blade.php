68a6bd69172e13076598ebac046c3db369d9509e
ivan <ivan@haxibiao.com>
2021-07-15 17:10:46 +0800

    修复影大全大量空专题封面的图片显示

--- resources/views/index/parts/recommend_categories.blade.php
+++ resources/views/index/parts/recommend_categories.blade.php
@@ -1,11 +1,11 @@
 <div class="recommend-category hidden-xs">
   @foreach($categories as $category)
     <a href="/category/{{ $category->id }}" class="category-label">
-      <img src="{{ $category->iconUrl }}" alt="{{ $category->name }}">
+      <img src="{{ $category->logo }}" alt="{{ $category->name }}">
       <span class="name">{{ $category->name }}</span>
     </a>
   @endforeach
   <a href="/category" class="category-label more">
     <span class="name">更多热门专题 <i class="iconfont icon-youbian"></i></span>
   </a>
-</div> 
+</div>
