97cfa10abca862842b0d8f20fcde5cdc03ca1df0
ivan <ivan@mac>
2021-08-15 17:40:56 +0800

    apps sites切换数据库后，seed不同的账户名

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@ namespace Database\Seeders;
 
 use Haxibiao\Breeze\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -20,17 +21,31 @@ class UserSeeder extends Seeder
             dd("请设置 网站中文名称 env('APP_NAME_CN')");
         }
 
-        $admin_email = "master@" . env('APP_DOMAIN');
+        $app_name    = env('APP_NAME');
+        $app_name_cn = env('APP_NAME_CN');
+
+        //apps sites切换数据库后，seed不同的账户名
+        $db_name = DB::connection()->getDatabaseName();
+        if ($db_name != $app_name) {
+            $app_name = $db_name;
+            //找到对应域名
+            $app_domain = array_get(config('cms.apps_db'), $app_name) ?? array_get(config('cms.sites_db'), $app_name);
+            //找到对应seo_site_name
+            $seo_site_name = array_get(config('seo.sites'), $app_domain);
+            if ($seo_site_name != $app_name_cn) {
+                $app_name_cn = $seo_site_name;
+            }
+        }
 
         $default_pass = env('DEFAULT_PASSWORD', 'dadada');
 
-        //删除冗余的admin新账户？
+        $admin_email = "master@" . $app_name;
+        //删除冗余的用admin邮箱的账户？
         foreach (User::whereEmail($admin_email)->get() as $admin) {
             if ($admin->id > 1) {
                 $admin->delete();
             }
         }
-
         //锁定id=1为super admin
         $admin = User::find(1);
         if (!$admin) {
@@ -41,7 +56,7 @@ class UserSeeder extends Seeder
         $admin->email   = $admin_email;
         $admin->account = '001'; //账户名指派
         $admin->phone   = null; //未绑定手机
-        $admin->name    = env('APP_NAME_CN');
+        $admin->name    = $app_name_cn;
         $pass           = $default_pass;
 
         $admin->password  = bcrypt($pass);
@@ -51,7 +66,7 @@ class UserSeeder extends Seeder
         echo "\n已初始化账户:" . $admin->email;
 
         //锁定id=2 为 小编
-        $editor_email = 'editor@' . env('APP_DOMAIN');
+        $editor_email = 'editor@' . $app_name;
         $editor       = User::find(2);
         if (!$editor) {
             $editor = User::firstOrNew([
@@ -61,7 +76,7 @@ class UserSeeder extends Seeder
         $editor->email     = $editor_email;
         $admin->account    = '002'; //账户名指派
         $admin->phone      = null; //未绑定手机
-        $editor->name      = env('APP_NAME_CN') . "小编";
+        $editor->name      = $app_name_cn . "小编";
         $pass              = $default_pass;
         $editor->password  = bcrypt($pass);
         $editor->api_token = str_random(60);
@@ -70,7 +85,7 @@ class UserSeeder extends Seeder
         echo "\n已初始化账户:" . $editor->email;
 
         //锁定id=3 为 测试用户
-        $user_email = 'user@' . env('APP_DOMAIN');
+        $user_email = 'user@' . $app_name;
         $user       = User::find(3);
         if (!$user) {
             $user = User::firstOrNew([
@@ -80,7 +95,7 @@ class UserSeeder extends Seeder
         $user->email     = $user_email;
         $admin->account  = '003'; //账户名指派
         $admin->phone    = null; //未绑定手机
-        $user->name      = env('APP_NAME_CN') . "用户";
+        $user->name      = $app_name_cn . "用户";
         $pass            = $default_pass;
         $user->password  = bcrypt($pass);
         $user->api_token = str_random(60);
