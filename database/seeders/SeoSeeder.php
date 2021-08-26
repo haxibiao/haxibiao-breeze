6df5802f91e96f24a44d419198baab6e481483c9
ivan <ivan@mac>
2021-08-26 23:29:07 +0800

    SeoSeeder 不覆盖站长已更新value

--- database/seeders/SeoSeeder.php
+++ database/seeders/SeoSeeder.php
@@ -13,27 +13,25 @@ class SeoSeeder extends Seeder
      */
     public function run()
     {
-        Seo::truncate();
-
         //TKD
         $item = Seo::firstOrCreate([
             'group' => 'TKD',
             'name'  => 'title',
         ]);
-        $item->value = '疯狂看美剧，快乐无极限';
+        $item->value = $item->value ?? '疯狂看美剧，快乐无极限';
         $item->save();
         $item = Seo::firstOrCreate([
             'group' => 'TKD',
             'name'  => 'keywords',
         ]);
-        $item->value = '精选美剧，告别剧荒，播放流畅，更新迅速，精彩短视频，抖音精彩电影剪辑合集';
+        $item->value = $item->value ?? '精选美剧，告别剧荒，播放流畅，更新迅速，精彩短视频，抖音精彩电影剪辑合集';
         $item->save();
 
         $item = Seo::firstOrCreate([
             'group' => 'TKD',
             'name'  => 'description',
         ]);
-        $item->value = '行尸走肉、西部世界、天赋异禀、黑袍纠察队、邪恶力量、暮光之城等精彩影视大片一网打尽，小编为您精心搜集了所有经典爆款、时下热门、最新上映的美剧资源，资源齐全更新快，全部支持免费在线观影！';
+        $item->value = $item->value ?? '行尸走肉、西部世界、天赋异禀、黑袍纠察队、邪恶力量、暮光之城等精彩影视大片一网打尽，小编为您精心搜集了所有经典爆款、时下热门、最新上映的美剧资源，资源齐全更新快，全部支持免费在线观影！';
         $item->save();
 
         //站长 - 验证
@@ -41,7 +39,7 @@ class SeoSeeder extends Seeder
             'group' => '站长',
             'name'  => 'meta',
         ]);
-        $item->value = '';
+        $item->value = $item->value ?? '';
         $item->save();
 
         //站长 - 提交
@@ -49,7 +47,7 @@ class SeoSeeder extends Seeder
             'group' => '站长',
             'name'  => 'token',
         ]);
-        $item->value = '';
+        $item->value = $item->value ?? '';
         $item->save();
 
     }
