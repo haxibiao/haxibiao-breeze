cd89dee50ef2a9151afbcae7d89b24428bc69e15
ivan <ivan@mac>
2021-09-07 16:43:14 +0800

    add enable_pushy

--- config/breeze.php
+++ config/breeze.php
@@ -6,4 +6,5 @@ return [
     'enable_mail_notification' => env('ENABLE_MAIL_NOTIFICATION', false),
     'enable_onesignal'         => false,
     'enable_pushalert'         => false,
+    'enable_pushy'             => false,
 ];
