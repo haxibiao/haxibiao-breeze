3b02b45d375a3da543b6b5c0a9ca214daaa5572b
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-09-18 10:16:50 +0800

    添加store和user关联关系

--- src/Traits/UserRelations.php
+++ src/Traits/UserRelations.php
@@ -23,6 +23,7 @@ use Haxibiao\Sns\Feedback;
 use Haxibiao\Sns\Follow;
 use Haxibiao\Sns\UserBlock;
 use Haxibiao\Store\Order;
+use Haxibiao\Store\Store;
 use Haxibiao\Task\Contribute;
 use Haxibiao\Wallet\Exchange;
 use Haxibiao\Wallet\Gold;
@@ -36,6 +37,11 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 trait UserRelations
 {
     //关系
+    //所属商铺
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
 
     //可服务项目
     public function services()
