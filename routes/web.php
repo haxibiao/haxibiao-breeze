c1eee46cd3672f1291774d389fe7985cf079c728
ivan <ivan@haxibiao.com>
2021-09-28 11:27:47 +0800

    fix serviceworker.js 404

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 // Scripts & Styles...
 Route::get('/css/{asset}', 'BreezeAssetController@show')->middleware(CheckResponseForModifications::class);
 Route::get('/js/{asset}', 'BreezeAssetController@show')->middleware(CheckResponseForModifications::class);
-// Route::get('/fonts/{asset}', 'BreezeAssetController@show')->middleware(CheckResponseForModifications::class);
+Route::get('/serviceworker.js', 'BreezeAssetController@show')->middleware(CheckResponseForModifications::class);
 Route::get('/images/{asset}', 'BreezeAssetController@show')->middleware(CheckResponseForModifications::class);
 Route::get('/images/{folder}/{asset}', 'BreezeAssetController@show')->middleware(CheckResponseForModifications::class);
 
