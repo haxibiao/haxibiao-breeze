363f28a877883c91bc50c35f98a8de11285c775d
xxm <xiaoxinming@haxibiao.com>
2021-10-25 00:33:22 +0800

    fixed:archive脚本添加耗时输出,修复计算平均数未考虑为0的条件,导致归档失败

--- src/Console/Dimension/ArchiveAll.php
+++ src/Console/Dimension/ArchiveAll.php
@@ -20,6 +20,8 @@ class ArchiveAll extends Command
      */
     protected $description = '每天凌晨归档所有维度(新增，留存，广告，提现，变现)';
 
+    protected $startedTime = null;
+
     /**
      * Create a new command instance.
      *
@@ -39,27 +41,60 @@ class ArchiveAll extends Command
     {
         $date = $this->option('date') ?? today()->toDateString();
 
-        //每日归档新用户数据
+        // //每日归档新用户数据
+        $this->recordStarted();
         $this->call("archive:user", ['--date' => $date, '--newuser' => true]);
+        $this->recordCompleted();
 
         //每日归档新老用户数据
+        $this->recordStarted();
         $this->call("archive:user", ['--date' => $date, '--categoryuser' => true]);
+        $this->recordCompleted();
 
         //每日归档新用户激活漏斗
+        $this->recordStarted();
         $this->call("archive:user", ['--date' => $date, '--newUserActivation' => true]);
+        $this->recordCompleted();
 
         //每日更新新用户激活漏斗中的次日留存率
+        $this->recordStarted();
         $this->call("archive:user", ['--date' => $date, '--updateNewUserActivation' => true]);
+        $this->recordCompleted();
 
         //每日更新分类用户平均答题数
+        $this->recordStarted();
         $this->call("archive:user", ['--date' => $date, '--avg' => true]);
+        $this->recordCompleted();
 
         //每日归档留存率
+        $this->recordStarted();
         $this->call("archive:retention", ['--date' => $date]);
+        $this->recordCompleted();
 
         //每日归档提现数据
+        $this->recordStarted();
         $this->call("archive:withdraw", ['--date' => $date]);
+        $this->recordCompleted();
+    }
 
+    public function recordStarted()
+    {
+        $this->startedTime = now();
+        $this->info(str_pad(" run ready now:$this->startedTime ", 60, '=', STR_PAD_BOTH));
+    }
+
+    public function recordCompleted()
+    {
+        $now            = now();
+        $processSeconds = $this->getPorcessSeconds($now);
+        $this->info(str_pad(" run completed now:$now total_seconds:$processSeconds ", 60, '=', STR_PAD_BOTH));
+    }
+
+    public function getPorcessSeconds($time)
+    {
+        $seconds = !is_null($this->startedTime) ? $time->diffInSeconds($this->startedTime) : 0;
+
+        return $seconds;
     }
 
 }
