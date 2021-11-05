199960459283225ffd8df14fc71e8554728b4048
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-11-05 16:30:46 +0800

    添加seo站点配置接口

--- src/Seo.php
+++ src/Seo.php
@@ -4,7 +4,6 @@ namespace Haxibiao\Breeze;
 
 use Illuminate\Database\Eloquent\Model;
 
-
 class Seo extends Model
 {
     protected $fillable = ['group', 'name', 'value'];
@@ -13,5 +12,16 @@ class Seo extends Model
     {
         $item = self::whereGroup($group)->whereName($name)->first();
         return $item ? $item->value : '';
+    }
+
+    public function resolveSeos($rootValue, $args, $context, $resolveInfo)
+    {
+        $name  = $args['name'] ?? null;
+        $group = $args['group'] ?? null;
+        return Seo::query()->when($name, function ($qb) use ($name) {
+            $qb->where('name', $name);
+        })->when($group, function ($qb) use ($group) {
+            $qb->where('group', $group);
+        })->get();
     }
 }
