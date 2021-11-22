3753ab85c2a69f121bd9a5a6227a10613a565dd7
ivan <ivan@work>
2021-11-22 14:57:28 +0800

    需要维护单独的pwa后端时覆盖window.gqlUri

--- resources/views/parts/js_for_pwa.blade.php
+++ resources/views/parts/js_for_pwa.blade.php
@@ -1,9 +1,13 @@
 {{-- 支持替换pwa模板自定义部分 --}}
 <script>
-    window.logoUrl = '{{ small_logo() }}';
-    window.logoTextUrl = '{{ text_logo() }}';
-    window.apkUrl = '{{ getApkUrl() }}';
-    window.downloadUrl = '{{ app_download_url() }}';
-    window.appDomain = '{{ app_domain() }}';
+
+{{-- 需要维护单独的pwa后端时覆盖window.gqlUri  --}}
+{{-- window.gqlUri = '/gql';  --}}
+
+window.logoUrl = '{{ small_logo() }}';
+window.logoTextUrl = '{{ text_logo() }}';
+window.apkUrl = '{{ getApkUrl() }}';
+window.downloadUrl = '{{ app_download_url() }}';
+window.appDomain = '{{ app_domain() }}';
 
 </script>
