48f84c2c69c383d79abfcd14bef6466e7e9fbc40
ivan <ivan@work>
2021-11-28 16:08:37 +0800

    整理seo tkd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
-@section('title')登录 -
-@stop
+@section('title')登录 @stop
 
 @php
 //登录成功返回之前的页面
@@ -11,7 +10,7 @@ session()->put('url.intended', request()->headers->get('referer'));
 @section('content')
 @include('parts.header_guest', ['auth' => true])
 <div id="login" style="padding-top:20px">
-    {{--  <div class="logo">
+    {{-- <div class="logo">
             <a href="/">
                 <img src="{{ text_logo() }}.png }}" alt="{{ config('app.name') }}">
     </a>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,35 +1,35 @@
 @extends('layouts.app')
 
-@section('title')注册 - @stop
+@section('title')注册@stop
 
 @section('content')
-    @include('parts.header_guest', ['auth' => true])
-    <div id="login" style="padding-top:20px">
-        {{--  <div class="logo">
+@include('parts.header_guest', ['auth' => true])
+<div id="login" style="padding-top:20px">
+    {{-- <div class="logo">
             <a href="/">
                 <img src="{{ text_logo() }}" alt="{{ config('app.name') }}">
-            </a>
-        </div>  --}}
+    </a>
+</div> --}}
 
-        <div class="sign-containers">
+<div class="sign-containers">
 
-            @include('auth.download_app')
+    @include('auth.download_app')
 
-            {{-- 登录注册vue --}}
-            <signs></signs>
+    {{-- 登录注册vue --}}
+    <signs></signs>
 
-        </div>
+</div>
 
-        @if($errors->any())
-        <div class="alert alert-danger">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <strong>出错了！</strong>
-            <ul>
-                @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-    </div>
+@if($errors->any())
+<div class="alert alert-danger">
+    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    <strong>出错了！</strong>
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+</div>
 @stop
