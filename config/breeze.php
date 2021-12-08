88dba04f80225ec0da25b71d544ba48687a8849d
蔡志刚 <caizhigang@haxibiao.com>
2021-12-08 14:42:11 +0800

    HXB-582 可关闭PC端的登陆入口

--- config/breeze.php
+++ config/breeze.php
@@ -1,8 +1,15 @@
 <?php
 
 return [
-    'migration_autoload'       => true,
-    'routes_autoload'          => true,
-    'enable_mail_notification' => env('ENABLE_MAIL_NOTIFICATION', false),
+    'migration_autoload'        => true,
+    'routes_autoload'           => true,
+    'enable_mail_notification'  => env('ENABLE_MAIL_NOTIFICATION', false),
+    /**
+     * 站点logo的模式串
+     */
     'logo_path_pattern'        => env('BREEZE_LOGO_PATH_PATTERN',null),
+    /**
+     * 是否关闭PC端的登陆入口
+     */
+    'disable_login_page'            => env('BREEZE_DISABLE_LOGIN_PAGE',true),
 ];
