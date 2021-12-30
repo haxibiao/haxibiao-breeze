7b4f416881e0b79565d2593ab8e78b1a7fdf9d95
ivan <ivan@haxibiao.com>
2021-12-30 19:42:57 +0800

    cloudflare:cache:purge 支持key email参数支持ops bash多账户

--- src/Console/Cdn/CloudfareCachePurge.php
+++ src/Console/Cdn/CloudfareCachePurge.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Haxibiao\Breeze\Console\Cdn;
 
 use Illuminate\Console\Command;
@@ -18,7 +17,7 @@ class CloudfareCachePurge extends Command
 {
     protected $name = 'cloudflare:cache:purge';
 
-    protected $signature = 'cloudflare:cache:purge
+    protected $signature = 'cloudflare:cache:purge {--cfkey= : cloudflare api key} {--cfemail= : cloudflare email}
         {--host=* : One or more hosts that should be removed from the cache.}';
 
     protected $description = 'Purge CloudFlare\'s cache.';
@@ -29,37 +28,42 @@ class CloudfareCachePurge extends Command
     public function __construct()
     {
         parent::__construct();
-        $this->cust_email = config('breeze.cloudfare.email');
-        $this->cust_xauth = config('breeze.cloudfare.key');
+
     }
 
     public function handle()
     {
+        $this->cust_email = $this->option('cfemail') ?? config('breeze.cloudfare.email');
+        $this->cust_xauth = $this->option('cfkey') ?? config('breeze.cloudfare.key');
+
         $hosts = $this->getHosts();
         $zones = $this->getZones();
-        foreach ($hosts as $cust_domain){
-            if($this->cust_email == "" || $this->cust_xauth == "" || $cust_domain == "") return;
+        foreach ($hosts as $cust_domain) {
+            if ($this->cust_email == "" || $this->cust_xauth == "" || $cust_domain == "") {
+                return;
+            }
+
             $cust_zone = $zones[$cust_domain];
-            if(blank($cust_zone)){
+            if (blank($cust_zone)) {
                 continue;
             }
             $ch_purge = curl_init();
-            curl_setopt($ch_purge, CURLOPT_URL, "https://api.cloudflare.com/client/v4/zones/".$cust_zone."/purge_cache");
+            curl_setopt($ch_purge, CURLOPT_URL, "https://api.cloudflare.com/client/v4/zones/" . $cust_zone . "/purge_cache");
             curl_setopt($ch_purge, CURLOPT_CUSTOMREQUEST, "DELETE");
             curl_setopt($ch_purge, CURLOPT_RETURNTRANSFER, 1);
             $headers = [
-                'X-Auth-Email: '.$this->cust_email,
-                'X-Auth-Key: '.$this->cust_xauth,
-                'Content-Type: application/json'
+                'X-Auth-Email: ' . $this->cust_email,
+                'X-Auth-Key: ' . $this->cust_xauth,
+                'Content-Type: application/json',
             ];
             $data = json_encode(array("purge_everything" => true));
             curl_setopt($ch_purge, CURLOPT_POST, true);
             curl_setopt($ch_purge, CURLOPT_POSTFIELDS, $data);
             curl_setopt($ch_purge, CURLOPT_HTTPHEADER, $headers);
 
-            $result = json_decode(curl_exec($ch_purge),true);
+            $result = json_decode(curl_exec($ch_purge), true);
             curl_close($ch_purge);
-            if(data_get($result,'success') == true){
+            if (data_get($result, 'success') == true) {
                 $this->info("$cust_domain purge successful");
             } else {
                 $this->error("$cust_domain purge fail");
@@ -67,40 +71,43 @@ class CloudfareCachePurge extends Command
         }
     }
 
-    function getHosts(){
+    public function getHosts()
+    {
         $hosts = $this->option('host');
-        if(!blank($hosts)){
+        if (!blank($hosts)) {
             return Arr::wrap($hosts);
         }
         return array_keys($this->getZones());
     }
 
-    function getZones($page = 1, $per_page = 100) {
-        $cache_key = sprintf('cloudflare_zones_%s',$this->cust_email);
-        return Cache::remember($cache_key, 60, function () use ($page, $per_page){
+    public function getZones($page = 1, $per_page = 100)
+    {
+        $cache_key = sprintf('cloudflare_zones_%s', $this->cust_email);
+        return Cache::remember($cache_key, 60, function () use ($page, $per_page) {
             $ch_query = curl_init();
             curl_setopt($ch_query, CURLOPT_URL, "https://api.cloudflare.com/client/v4/zones?status=active&page=$page&per_page=$per_page&order=status&direction=desc&match=all");
             curl_setopt($ch_query, CURLOPT_RETURNTRANSFER, 1);
             $qheaders = array(
-                'X-Auth-Email: '.$this->cust_email.'',
-                'X-Auth-Key: '.$this->cust_xauth.'',
-                'Content-Type: application/json'
+                'X-Auth-Email: ' . $this->cust_email,
+                'X-Auth-Key: ' . $this->cust_xauth,
+                'Content-Type: application/json',
             );
             curl_setopt($ch_query, CURLOPT_HTTPHEADER, $qheaders);
-            $qresult = json_decode(curl_exec($ch_query),true);
+            $curl_result = curl_exec($ch_query);
+            $qresult     = json_decode($curl_result, true);
             curl_close($ch_query);
 
-            if($qresult['success'] !=true){
+            if (Arr::get($qresult, 'success') != true) {
                 throw new \Exception("not success" . ($qresult['errors'] ?? '') . ($qresult['messages'] ?? ''));
             }
             $result = $qresult['result'];
-            if(blank($result)){
+            if (blank($result)) {
                 throw new \Exception("result null");
             }
 
             return array_combine(
-                data_get($result,'*.name'),
-                data_get($result,'*.id')
+                data_get($result, '*.name'),
+                data_get($result, '*.id')
             );
         });
     }
